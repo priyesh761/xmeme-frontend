2ca6d1063c383a79b8b955c0c7d0cc3079182447
priyesh761 <shettypriyesh761@gmail.com>
2021-02-15 18:16:55 +0530

    m

--- api_call.js
+++ api_call.js
@@ -1,6 +1,6 @@
 //import axios from "axios";
 
-const uri = "https://54.160.73.49:8081";
+const uri = "http://54.160.73.49:8081";
 //const uri = "http://127.0.0.1:8081";
 
 // Return latest 100 memes
@@ -19,13 +19,9 @@ const get_id = async (id) => {
 };
 
 // Add new meme to meme stream
-const post = async (obj) => {
+const post = async (payload) => {
   return await axios
-    .post(uri + "/memes/", {
-      name: obj.name,
-      caption: obj.caption,
-      url: obj.url,
-    })
+    .post(uri + "/memes ", payload)
     .catch((err) => console.log(err));
 };
 
