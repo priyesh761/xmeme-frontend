000a0d35d3c68910abd986a87bd9b26a2b255d68
priyesh761 <shettypriyesh761@gmail.com>
2021-04-17 23:07:14 +0530

    implemented lazy load and paging

--- api_call.js
+++ api_call.js
@@ -1,13 +1,32 @@
 const uri = "https://xmeme-backend-v2.herokuapp.com";
 //const uri = "http://127.0.0.1:8081";
 
-// Return latest 100 memes
+const isInt = (value) =>
+  !isNaN(value) &&
+  (function (x) {
+    return (x | 0) === x;
+  })(parseFloat(value));
+
+// Return latest 20 memes
 const get = async () => {
   // console.log("debug");
   return await fetch(uri + "/memes")
     .then((res) => res.json())
     .then((dta) => dta)
     .catch((err) => console.log(err.message));
+};
+
+// Return provided page number of memes
+// Per page 20 memes present
+const get_page = async (pageNo) => {
+  // console.log("debug");
+  if (1 && isInt(pageNo))
+    return await fetch(uri + "/memes" + "?pageNo=" + pageNo)
+      .then((res) => res.json())
+      .then((dta) => dta)
+      .catch((err) => console.log(err.message));
+
+  return [];
 };
 
 // Return memes with given id
--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 let wall = document.querySelector("#wall");
 
-const updateWall = () => {
-  wall.innerHTML = "";
+// Observer to track scroll area for lazy load
+let observer = new IntersectionObserver(function (entries, self) {
+  // iterate over each entry
+  entries.forEach((entry) => {
+    // process just the images that are intersecting.
+    // isIntersecting is a property exposed by the interface
+    if (entry.isIntersecting) {
+      // custom function that copies the path to the img
+      // from data-src to src
+      entry.target.setAttribute("src", entry.target.getAttribute("data-src"));
+      // the image is now in place, stop watching
+      self.unobserve(entry.target);
+    }
+  });
+});
 
-  get().then((data) =>
+const updateWall = async () => {
+  var sub = 0;
+  if (pageNo < 0) return 1;
+
+  await get_page(pageNo).then((data) => {
+    if (data == null || data.length == 0) {
+      sub = 1;
+      return;
+    }
+    wall.innerHTML = "";
+
     data.forEach((element) => {
       //  console.log(element);
       wall.innerHTML += `
-    <div class="m-5">
-        <div id=${element._id} class="card rounded col-12 col-md-6 col-lg-4 offset-md-2 offset-lg-3">
+        <div class="m-5">
+          <div id=${element._id} class="card rounded col-12 col-md-6 col-lg-4 offset-md-2 offset-lg-3">
             <div class="card-header bg-white d-flex justify-content-between">
             <h5 style="display:inline">${element.name}</h5>
             <button class="btn rounded-circle ml-auto" onclick="location.href = 'edit.html?id=${element._id}';"}>
@@ -25,13 +48,22 @@ const updateWall = () => {
           </button>
         
             </div>
-            <img src="${element.url}" alt="image not found" class="card-img-top" alt="...">
+            <img src="loading.svg" data-src="${element.url}" alt="image not found" class="card-img-top" alt="loading..." >
             <div class="card-body">
                 <p class="card-text">${element.caption}</div>
             </div>
         </div>
     </div>
     `;
-    })
-  );
+    });
+
+    const imgs = document.querySelectorAll("[data-src]");
+    imgs.forEach((img) => observer.observe(img));
+
+    // scroll to top after adding content
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  });
+
+  return sub;
 };
